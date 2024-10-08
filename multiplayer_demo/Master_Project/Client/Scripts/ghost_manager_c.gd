4152653641b654748755aa0cb86a824c13ebf343
Derek Dorr <dhdorr@gmail.com>
2024-10-07 22:26:50 -0700

    added entity interpolation v1
    added a rough basic version of entity interpolation

--- multiplayer_demo/Master_Project/Client/Scripts/ghost_manager_c.gd
+++ multiplayer_demo/Master_Project/Client/Scripts/ghost_manager_c.gd
@@ -6,22 +6,30 @@ const PLAYER_S = preload("res://Master_Project/Server/Scenes/player_s.tscn")
 
 var ghost_dict : Dictionary
 
+var prev_ghost_pos_dict : Dictionary
+
 
 func spawn_peer_characters_2(world_state: Dictionary) -> void:
 	for p_id : int in world_state:
 		if p_id == connection_manager_c.player_id:
 			continue
-		var temp_vec : Vector2 = world_state[p_id]["position"]
 		
 		if !ghost_dict.has(p_id):
 			ghost_dict[p_id] = PLAYER_S.instantiate()
 			add_child(ghost_dict[p_id])
+			
+			prev_ghost_pos_dict[p_id] = world_state[p_id]["position"]
 		
 		move_ghost(world_state[p_id], p_id)
 
-
+# Entity interpolation
 func move_ghost(player: Dictionary, p_id : int) -> void:
-	ghost_dict[p_id].position = player["position"]
+	#ghost_dict[p_id].position = player["position"]
+	
+	ghost_dict[p_id].position = prev_ghost_pos_dict[p_id]
+	var tween := get_tree().create_tween()
+	tween.tween_property(ghost_dict[p_id],"position", player["position"], SettingsMp.get_server_tick_rate() * get_physics_process_delta_time())
+	prev_ghost_pos_dict[p_id] = player["position"]
 
 # Ghost manager should have its own input buffer for packets (fed from the
 # buffer manager) to make it easier to interpolate entities independantly
