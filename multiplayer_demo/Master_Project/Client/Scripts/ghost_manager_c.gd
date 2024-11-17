c88c7fbfc7cda2644ca7e6df9b0adaa7ebdc6ef7
Derek Dorr <dhdorr@gmail.com>
2024-11-16 21:43:39 -0800

    refactor ghost manager
    player ghost scene handles the ghosts own movement

--- multiplayer_demo/Master_Project/Client/Scripts/ghost_manager_c.gd
+++ multiplayer_demo/Master_Project/Client/Scripts/ghost_manager_c.gd
@@ -26,26 +26,10 @@ func spawn_peer_characters_2(world_state: Dictionary) -> void:
 
 # Entity interpolation
 func move_ghost(player: Dictionary, p_id: int) -> void:
-	#ghost_dict[p_id].position = player["position"]
-	#ghost_dict[p_id].position = prev_ghost_pos_dict[p_id]
-	if SettingsMp.enable_client_entity_interpolation:
-		interpolate_ghosts(player, p_id)
-	else:
-		ghost_dict[p_id].position = player["position"]
-		#prev_ghost_pos_dict[p_id] = player["position"]
-		update_prev_ghost_pos(p_id, player["position"])
-	
-
-func interpolate_ghosts(player: Dictionary, p_id: int) -> void:
 	var ghost : PLAYER_GHOST_CLIENT = ghost_dict[p_id]
-	#var tween := get_tree().create_tween()
-	
-	#tween.tween_property(ghost_dict[p_id],"position", player["position"], SettingsMp.get_server_tick_rate() * get_physics_process_delta_time())
-	
 	ghost._skin_rotation =  player["skin_rotation"]
 	ghost._target_position = player["position"]
-	
-	#tween.finished.connect(update_prev_ghost_pos.bind(p_id, player["position"]))
+	update_prev_ghost_pos(p_id, player["position"])
 	
 
 
--- multiplayer_demo/Master_Project/Client/Scripts/player_ghost_c_3d.gd
+++ multiplayer_demo/Master_Project/Client/Scripts/player_ghost_c_3d.gd
@@ -10,9 +10,15 @@ func _physics_process(delta: float) -> void:
 	move_ghost(delta)
 
 func rotate_ghost(delta: float) -> void:
-	character_soldier_01.rotation.y = lerp_angle(
-		character_soldier_01.rotation.y, _skin_rotation.y, SettingsMp.get_server_tick_rate() * delta
-	)
+	if SettingsMp.enable_client_entity_interpolation:
+		character_soldier_01.rotation.y = lerp_angle(
+			character_soldier_01.rotation.y, _skin_rotation.y, SettingsMp.get_server_tick_rate() * delta
+		)
+	else:
+		character_soldier_01.rotation.y = _skin_rotation.y
 
 func move_ghost(delta: float) -> void:
-	self.position = self.position.lerp(_target_position, SettingsMp.get_server_tick_rate() * delta)
+	if SettingsMp.enable_client_entity_interpolation:
+		self.position = self.position.lerp(_target_position, SettingsMp.get_server_tick_rate() * delta)
+	else:
+		self.position = _target_position
